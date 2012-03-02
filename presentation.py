2092c2b31acd7f083ba12d848f223f459bfaeefb
Kiril Vladimiroff <v.kiril@gmail.com>
2012-03-03 00:51:54 +0200

    Simple presentation generator.
    P.S. I'm still not sure this needs to be a class...

---
+++ presentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+import shpaml
+
+class Presentation:
+    def __init__(self, input_file, lecture_name, date, *args):
+        self.input_file = input_file
+        self.date = date
+
+    @property
+    def header(self):
+        with open('ui/header.html') as f:
+            return f.read().format(self.lecture_name, self.date)
+
+    @property
+    def footer(self):
+        with open('ui/footer.html') as f:
+            return f.read()
+
+    @property
+    def slides(self):
+        with open(self.input_file) as f:
+            return shpaml.convert_text(f.read())
+
+    def render(self):
+        return self.header + self.slides + self.footer
+
+
+if __name__ == '__main__':
+    presentation = Presentation(*sys.argv[1:])
+    print(presentation.render())
