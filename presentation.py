52118c23b053a4e61c541ad47cd847c51cbbfbd9
Kiril Vladimiroff <v.kiril@gmail.com>
2012-03-03 01:14:46 +0200

    Okay, now I'm sure. There is now obvious reason for presentation to be a class

--- presentation.py
+++ presentation.py
@@ -2,31 +2,15 @@
 import sys
 import shpaml
 
-class Presentation:
-    def __init__(self, input_file, lecture_name, date, *args):
-        self.input_file = input_file
-        self.lecture_name = lecture_name
-        self.date = date
+def build(input_file, lecture_name, date):
+    with open('ui/header.html') as f:
+        header = f.read().format(title=lecture_name, date=date)
+    with open('ui/footer.html') as f:
+        footer = f.read()
+    slides = shpaml.convert_text(f.read())
 
-    @property
-    def header(self):
-        with open('ui/header.html') as f:
-            return f.read().format(title=self.lecture_name, date=self.date)
-
-    @property
-    def footer(self):
-        with open('ui/footer.html') as f:
-            return f.read()
-
-    @property
-    def slides(self):
-        with open(self.input_file) as f:
-            return shpaml.convert_text(f.read())
-
-    def render(self):
-        return self.header + self.slides + self.footer
+    return header + slides + footer
 
 
 if __name__ == '__main__':
-    presentation = Presentation(*sys.argv[1:])
-    print(presentation.render())
+    print(build(sys.argv[1:]))
